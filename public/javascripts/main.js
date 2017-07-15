0c4007ae1d4739a22a8e984ed578e223f63c10c1
Bowen Fan <bowenfan32@gmail.com>
2017-07-15 20:40:34 +1000

    Add additional ticket list info and stylize with Bootstrap table

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
 
    	$(".ticket-list").paginate({ 'perPage': 25 });
-   	$(".ticket-list").paginate({ 'scope': $('li') });
+   	$(".ticket-list").paginate({ 'scope': $('tr') });
    	// $(".ticket-list").data('paginate').switchPage(1);
 	$(".ticket-list").data('paginate').switchPage('next'); //next page
 	$(".ticket-list").data('paginate').switchPage('prev'); //previous page
+
+
+	$(".clickable-row").click(function() {
+        window.location = $(this).data("href");
+    });
 
 });
   
