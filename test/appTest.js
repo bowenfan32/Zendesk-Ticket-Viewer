6d82f448ae675e368516abd952db77c72a110b7c
Bowen Fan <bowenfan32@gmail.com>
2017-07-20 17:27:35 +1000

    Add authentication tests

--- test/appTest.js
+++ test/appTest.js
@@ -2,6 +2,9 @@ const assert = require('chai').assert;
 var request = require('request');
 var expect = require('chai').expect;
 const app = require('../app');
+var zendesk = require('node-zendesk');
+
+
 
 describe('GET /', function() {
 	it('should return 200', function (done) {
@@ -20,3 +23,35 @@ describe('GET Invalid page', function() {
   		});
 	});
 });
+
+var client = zendesk.createClient({
+  username:  process.env.EMAIL,
+  token:  process.env.TOKEN,
+  remoteUri: process.env.URL
+});
+describe('Return 200 with correct auth', function() {
+  this.timeout(15000);
+  it('should return 200', function(done) {
+    client.tickets.list(function (err, response, body) {
+      console.log(response[0]);
+      expect(response[0]).to.equal(200);
+        done();
+    });
+  })
+})
+
+
+var client1 = zendesk.createClient({
+  username:  process.env.EMAIL,
+  token:  "process.env.TOKEN",
+  remoteUri: process.env.URL
+});
+describe('Return err with incorrect auth', function() {
+  this.timeout(15000);
+  it('should return 401', function(done) {
+    client1.tickets.list(function (err, response, body) {
+      expect(err)
+        done();
+    });
+  })
+})
